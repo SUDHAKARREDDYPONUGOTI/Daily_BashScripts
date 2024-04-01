6a2f31ac7daa9000cded18d155bb8439f74cd4d4
SUDHAKARREDDYPONUGOTI <ponugotisudhakar468@gmail.com>
2024-04-01 11:38:58 +0530

    changes

--- Shell_Scripts/File_backUp.sh
+++ Shell_Scripts/File_backUp.sh
@@ -2,8 +2,8 @@
 
 echo "Hello Sudhakar"
 
-backup_dir = "cd destFile/"
-source_dir = "cd sourceFile/"
+backup_dir = "/home/centos/destFile/"
+source_dir = "/home/centos/sourceFile/"
 
 tar -czf "$backup_dir/backup_$(date
 +%Y%m%d_%H%M%S).tar.gz"
